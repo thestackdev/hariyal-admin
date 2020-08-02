93d5e94fc59cd29166584610941259196f0de80f
GauthamAsir <gauthamasir@gmail.com>
2020-08-02 23:24:43 +0530

    updated logo, splash screen & launcher icon
    Signed-off-by: GauthamAsir <gauthamasir@gmail.com>

--- lib/initial_page.dart
+++ lib/initial_page.dart
@@ -3,16 +3,15 @@ import 'package:flutter/material.dart';
 class InitialRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.black,
-        image: DecorationImage(
-          fit: BoxFit.contain,
-          alignment: Alignment.center,
-          image: AssetImage('assets/hariyal.png'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'assets/hariyal.png',
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.fill,
         ),
       ),
-      child: Center(),
     );
   }
 }
